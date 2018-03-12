5049f6e7ff71dba2fb02a0f2da09ce88d1813e7c
JSmith146 <mrjeffreysmithjr@gmail.com>
2018-03-12 14:08:25 -0400

    update

---
+++ Articles.R
@@ -0,0 +1,18 @@
+#'  April 18 - August 31, 2017 News Articles
+#'
+#'  A clean dataset containing 82,688 news articles from nine 
+#'    recognized news sources collected between April 18 - August 31, 2017. 
+#'  
+#'  \describe{ 
+#'    \item{Title}{The title of each news article} 
+#'    \item{Author}{Identifies the
+#'    author of each news article. Unidentified authors are represented by
+#'    "unknown" } 
+#'    \item{Date}{Publication date of each news article given in the
+#'    "YYYYMMDD HH:MM:SS" format} 
+#'    \item{URL}{Weblink to each news article} 
+#'    \item{NewsSource}{Represents the name of the news source where each
+#'    article was published} 
+#'    \item{Text}{Text from each news article} 
+#'    \item{ArticleNo}{Sequential list of each article in the dataset} }
+"Articles"
